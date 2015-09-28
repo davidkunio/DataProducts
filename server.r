4481dc6e03b23e5e8ac800945e6d7f5199d8fc89
davidkunio <davidkunio@gmail.com>
2015-09-27 20:34:39 -0500

    final fixes

--- server.r
+++ server.r
@@ -3,13 +3,12 @@ library(shiny)
 wd.datapath = paste0(getwd(),"/data")
 wd.init = getwd()
 setwd(wd.datapath)
-
+#teamlistinitial <- read.csv('standings.csv')
 teamlistinitial <- read.csv('standings.csv',stringsAsFactors=FALSE)
-
 setwd(wd.init)
 
 shinyServer(function(input, output) {
-  data <- reactive({ 
+  team <- reactive({ 
     LG <- switch(input$league,
                   "National" = 'NL',
                   "American" = 'AL')
@@ -17,16 +16,15 @@ shinyServer(function(input, output) {
                  "Yes" = 10,
                  "No" = 32)
     teamlist <- teamlistinitial
-    teamlist <- subset(teamlist,teamlist$Lg==LG)
-    teamlist <- subset(teamlist,teamlist$W > input$wins)
-    teamlist <- subset(teamlist,Rk < PLAY)
-    team <- teamlist$Tm[sample(1:length(teamlist$Tm),1)]
+    teamlist <- teamlist[teamlist$Lg==LG,]
+    teamlist <- teamlist[teamlist$W > input$wins,]
+    teamlist <- teamlist[teamlist$Rk < PLAY,]
+    teamname <- teamlist$Tm[sample(1:length(teamlist$Tm),1)]
+    return(teamname)
   })
-  output$image <- renderImage({
-    if (is.null(input$league))
-      return(NULL)
-    src_link <- paste('images/',team,'.jpg',sep='')
-    alt_txt <- team
+  output$team_image <- renderImage({
+    src_link <- paste('images/',team(),'.jpg',sep='')
+    alt_txt <- team()
     return(list(
         src = src_link,
         contentType = "image/jpg",
--- ui.r
+++ ui.r
@@ -1,6 +1,6 @@
 library(shiny)
 
-shinyUI(pageWithSidebar(
+shinyUI(fluidPage(
   headerPanel("For Whom Should I Cheer?"),
   sidebarPanel(
     selectInput("league", "Which League?", 
@@ -8,9 +8,9 @@ shinyUI(pageWithSidebar(
                            "American")),
     sliderInput('wins',"Minimun Number of Wins",min=1,max=100,value=50),
     radioButtons("playoffs", "Headed for the Playoffs?",
-                 choices = c('Yes','Does not Matter'), selected = NULL, inline = FALSE)
+                 choices = c('Yes','No'), selected = NULL, inline = FALSE)
     ),
   mainPanel(
-    imageOutput('image')
+    imageOutput('team_image')
   )
 ))
